55f6918ef97c3b9363d4cb7d116139e9b1613038
Dantevils <Elite@10.3.8.84>
2014-04-09 20:37:19 -0300

    Inicio de prototipo

---
+++ Taller/src/Motor.java
@@ -0,0 +1,37 @@
+// @utor: Dante Perez Aguilera
+
+
+public class Motor {
+
+	protected int Cilindros;/*Numero de Cilindros*/	
+	protected String Tec_inyection; /*Carburacion, Mono-inyector, inyecion Multiple (Por cilidro)*/
+	protected int Torque;/*Distribucion de indice Motor a traccion*/
+	protected int HP;/*Caballos de fuerza del Motor*/
+	protected String AC; /*yes or not ; Aire Acondicionado*/
+	protected String DA;/*yes or not ; Direccion Hidraulica*/
+	protected int Code_Motor;/*Numero de Placa de Motor*/
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Modelo del vehiculo ");
+
+		
+		
+	}
+
+}
+///Algunos puntos a considerar son la interfaz grafica con imagen o partes de ciertas clases, enton
+/*
+ 
+ * Interfaces Graficas,"Texto o imagen con diseño de CLASES(MOTOR,CHASIS"Tipo citycar")" 
+ *
+ *
+ *
+ *
+ */
+ */
